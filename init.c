bdf318e222ef4bf8f95364e48aefa281b4ccf6c5
Hind <hlichir@icloud.com>
2021-09-13 18:04:06 +0100

    init màj

--- init.c
+++ init.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
+/*   By: hlichir <hlichir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/02 11:52:56 by anadege           #+#    #+#             */
-/*   Updated: 2021/09/02 12:03:36 by anadege          ###   ########.fr       */
+/*   Updated: 2021/09/13 18:03:53 by hlichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ int	init_minishell(t_infos *infos, char **env)
 {
 	infos->prompt = NULL;
 	infos->curr_cmd = NULL;
+	infos->lst_var = NULL;
+	infos->last_exit_status = 0;
 	infos->fd_history = 0;
 	infos->env = NULL;
 	infos->fd_history = get_previous_history();

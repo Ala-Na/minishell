b4e26d4be7270f1b9f5efc08661096aad71006a5
anadege <ilmefautuneadresse@gmail.com>
2021-09-19 23:31:07 +0200

    Slowly progressing

--- exec/exec_cmd.c
+++ exec/exec_cmd.c
@@ -6,39 +6,58 @@
 /*   By: hlichir <hlichir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 15:00:10 by anadege           #+#    #+#             */
-/*   Updated: 2021/09/17 23:34:45 by anadege          ###   ########.fr       */
+/*   Updated: 2021/09/19 23:30:51 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
-
+/*
+char	**get_exec_args(t_infos *infos, t_cmd *cmd, t_token *exec_path)
+{
+	char	**exec_args;
+}
+*/
+/*
+** Function which return token corresponding to executable absolute or relative path
+** and register assignments variables inside exec_env.
+** Return NULL if error.
+*/
 t_token	*move_to_exec(t_infos *infos, t_cmd *cmd, char ***exec_env)
 {
-	if (copy_env(infos, infos->env, exec_env) < 0)
-		return (NULL); //gestion exit status dans copy_env
-	while (cmd->start != cmd->end && cmd->start->type == ASSIGNMENT)
+	int	diff_exec_env_size;
+
+	diff_exec_env_size = get_exec_env_diff_size(infos, cmd);
+	if (copy_env(infos, infos->env, exec_env, diff_exec_env_size) < 0)
+		return (NULL);
+	while (cmd->start->type == ASSIGNMENT)
 	{
 		if (add_elem_to_exec_env(infos, exec_env, cmd->start) < 0)
 		{
 			free_env(*exec_env, -1);
-			return (NULL); // gestion exit status dans add_elem_to_exec_env
+			return (NULL);
 		}
+		if (cmd->start == cmd->end)
+			break ;
 		cmd->start = cmd->start->next;
 	}
-	if (cmd->start->type == ASSIGNMENT)
-		return (NULL); // pas de gestion exit status
 	return (cmd->start);
 }
 
-
-char	*get_exec(t_infos *infos, t_cmd *cmd, char ***exec_env)
+/*
+** Function which return exec_path and set up the executable environment
+** variables inside exec_env.
+** Return NULL if an error occurs.
+*/
+char	*get_exec_path(t_infos *infos, t_cmd *cmd, char ***exec_env)
 {
 	t_token	*exec_token;
+	char	*path;
 
 	exec_token = move_to_exec(infos, cmd, exec_env);
 	if (!exec_token)
 		return (NULL);
-	//get path et return path
+	path = ft_strndup(exec_token->token, exec_token->length);
+	return (get_path(path, *exec_env));
 }
 
 /*
@@ -55,14 +74,18 @@ int	execute_simple_cmd(t_infos *infos)
 		return (-1); //erreur fork, cf errno
 	else if (pid > 0)
 	{
-		//On est dans le parent
 		waitpid(pid, &wstatus, 0);
 		kill(pid, SIGTERM);
 	}
 	else
 	{
-		if (execve(filename, args, infos->env) == -1)
+		exec_path = get_exec_path(infos, cmd, &exec_env);
+		exec_args = get_exec_args(infos, cmd, exec_path);
+
+		if (execve(filename, args, exec_env) == -1)
 			//print message erreur; //Cas ou execution a echoue, + errno
+		free(exec_path);
+		free_env(exec_env, -1);
 		exit(EXIT_FAILURE); //On arrive ici que si execve a échoué, car normalement 
 		//le child est exit quand execve est successfull
 	}

561c78c7d3e85230e545dff48ea457cc9c943a37
anadege <ilmefautuneadresse@gmail.com>
2021-09-19 23:31:07 +0200

    Slowly progressing

--- exec/set_env_exec.c
+++ exec/set_env_exec.c
@@ -6,7 +6,7 @@
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/17 16:53:57 by anadege           #+#    #+#             */
-/*   Updated: 2021/09/17 23:29:34 by anadege          ###   ########.fr       */
+/*   Updated: 2021/09/19 17:26:34 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,31 @@
 
 /*
 ** Function which add a new element to char **exec_env.
+** Strategy is slightly different from add_elem_to_env when the element
+** doesn't exist because here exec_env is supposed to already have the right
+** size.
 ** Returns -1 if an errors occurs, 0 otherwise.
 */
 int	add_elem_to_exec_env(t_infos *infos, char ***exec_env, t_token *new_elem)
 {
-	int		env_size;
+	int		i;
 	char	*elem_name;
 	int		res;
 
 	if (!infos || !(*exec_env) || !new_elem)
 		return (error_exit_status("Error!", infos, "?=1"));
-	env_size = 0;
 	res = -1;
-	while ((*exec_env)[env_size])
-		env_size++;
+	while (*exec_env[i])
+		i++;
 	elem_name = get_elem_name(new_elem->token, new_elem->length);
 	if (!elem_name)
 		return (error_exit_status("Memory allocation error", infos, "?=1"));
 	if (!get_env_elem(*exec_env, elem_name))
-		res = add_not_existing_elem_to_env(exec_env, new_elem, env_size);
+	{
+		*exec_env[i] = ft_strndup(new_elem->token, new_elem->length);
+		if (!*exec_env[i])
+			return (error_exit_status("Memory allocation error", infos, "?=1"));
+	}
 	else
 		res = modify_existing_elem_to_env(infos, *exec_env, new_elem,
 			elem_name);
@@ -41,36 +47,60 @@ int	add_elem_to_exec_env(t_infos *infos, char ***exec_env, t_token *new_elem)
 }
 
 /*
+** Function to calculate the positive difference between minishell
+** env and executable env.
+*/
+int	get_exec_diff_size(t_infos *infos, t_cmd *cmd)
+{
+	char	**env;
+	int		diff_size;
+	char	*elem_name;
+
+	diff_size = 0;
+	env = infos->env;
+	while (cmd->start->type == ASSIGNMENT)
+	{
+		elem_name = get_elem_name(cmd->start->token, cmd->start->length);
+		if (!elem_name)
+			return (error_exit_status("Memory allocation error", infos, "?=1"));
+		if (!get_env_elem(env, elem_name))
+			diff_size++;
+		free(elem_name);
+		cmd->start = cmd->start->next;
+	}
+	return (diff_size);
+}
+
+/*
 ** Function to copy the minishell char **env variable for the command
 ** executed, which is susceptible to contains a largest env.
 ** Allocations are realised to free the array without complications.
 ** Return -1 if an error occurs, 0 otherwise.
 */
-int	copy_env(t_infos *infos, char **env, char ***cpy_env)
+int	copy_env(t_infos *infos, char **env, char ***cpy_env, int cpy_diff_size)
 {
 	int		i;
 	int		y;
-	char	**tmp_env;
 
 	i = 0;
 	while (env && env[i])
 		i++;
-	tmp_env = malloc(sizeof(*tmp_env) * (i + 1));
-	if (!tmp_env)
+	*cpy_env = malloc(sizeof(**cpy_env) * (i + 1 + cpy_diff_size));
+	if (!*cpy_env)
 		return (error_exit_status("Malloc error", infos, "?=1"));
 	i = 0;
 	while (env[i])
 	{
 		y = 0;
-		tmp_env[i] = ft_strdup(env[i]);
-		if (!tmp_env[i])
+		*cpy_env[i] = ft_strdup(env[i]);
+		if (!*cpy_env[i])
 		{
-			free_env(tmp_env, i);
+			free_env(*cpy_env, i);
 			return (error_exit_status("Malloc error", infos, "?=1"));
 		}
 		i++;
 	}
-	tmp_env[i] = NULL;
-	*cpy_env = tmp_env;
+	while (i < i + 1 + cpy_diff_size)
+		*cpy_env[i++] = NULL;
 	return (0);
 }

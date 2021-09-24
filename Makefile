9eb9fbe5cbe4099697db5f6276b398af64d367db
Hind <hlichir@icloud.com>
2021-09-24 22:07:19 +0200

    unset invalid identifier gestion done

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: hlichir < hlichir@student.42.fr>           +#+  +:+       +#+         #
+#    By: hlichir <hlichir@student.42.fr>            +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/08/23 15:04:29 by anadege           #+#    #+#              #
-#    Updated: 2021/09/22 21:02:20 by anadege          ###   ########.fr        #
+#    Updated: 2021/09/24 22:05:12 by hlichir          ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,6 +19,7 @@ SRCS	= ./main.c \
 		  ./builtins/env.c \
 		  ./builtins/export.c \
 		  ./builtins/unset.c \
+		  ./builtins/unset_utils.c \
 		  ./builtins/echo.c \
 		  ./builtins/env_utils.c \
 		  ./builtins/pwd.c \
---
+++ builtins/unset_utils.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset_utils.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hlichir <hlichir@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/01 21:57:01 by anadege           #+#    #+#             */
+/*   Updated: 2021/09/24 22:03:42 by hlichir          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+/*
+** Function used to print when a invalid identifier is encountered.
+*/
+int	invalid_token_print(t_token *token)
+{
+	write(2, "unset: « ", 10);
+	write(2, token->token, token->length);
+	write(2, " » : not a valid identifier\n", 29);
+	return (-1);
+}
+
+/*
+** Function to check if the argument to unset is valid or not.
+** 1. Check if all the characters are '_' -> valid
+** 2. Check if all the characters are either 0-9 or '_' -> invalid
+** 3. Check if all the characters are either alphanumeric or '_' -> valid
+** 4. If the third check fails -> invalid
+*/
+int	check_validity_token(t_token *token)
+{
+	int	i;
+	int	l;
+
+	l = token->length;
+	i = 0;
+	while (i < l && token->token[i] == '_')
+		i++;
+	if (i == l)
+		return (0);
+	i = 0;
+	while (i < l && (token->token[i] == '_' || ft_isdigit(token->token[i])))
+		i++;
+	if (i == l)
+		return (invalid_token_print(token));
+	i = 0;
+	while (i < l && (token->token[i] == '_' || ft_isalnum(token->token[i])))
+		i++;
+	if (i == l)
+		return (0);
+	return (invalid_token_print(token));	
+}

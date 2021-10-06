49c6c152df886d5e85bcb69729a80934d2f84998
anadege <ilmefautuneadresse@gmail.com>
2021-10-06 10:20:31 +0200

    Sans int

--- exec/skip_redir.c
+++ exec/skip_redir.c
@@ -6,7 +6,7 @@
 /*   By: anadege <anadege@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/03 21:22:56 by anadege           #+#    #+#             */
-/*   Updated: 2021/10/05 18:07:47 by anadege          ###   ########.fr       */
+/*   Updated: 2021/10/06 10:20:20 by anadege          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,11 +58,11 @@ int	check_if_end_pipeline(t_cmd *cmd, t_token *token)
 ** pipeline when the last token was a redirection instruction.
 ** Returns -1 in case of error, 0 otherwise.
 */
-int	get_next_token_loop(t_cmd **cmd, t_token **token, int prev_was_redir)
+int	get_next_token_loop(t_cmd **cmd, t_token **token)
 {
 	if (!cmd || !token || !*cmd || !*token)
 		return (return_error(1, "something went wrong", 0, -1));
-	while (prev_was_redir)
+	while (1)
 	{
 		if (check_if_end_pipeline(*cmd, *token) != 0)
 		{
@@ -92,10 +92,8 @@ int	get_next_token_loop(t_cmd **cmd, t_token **token, int prev_was_redir)
 ** the first call but the cmd was empty, 1 if it was the first call and that
 ** the cmd didn't start by a redirection.
 */
-int	check_if_first_call(t_cmd *head_cmd, t_cmd **prev_cmd, t_token **curr_token,
-		int *prev_was_redir)
+int	check_if_first_call(t_cmd *head_cmd, t_cmd **prev_cmd, t_token **curr_token)
 {
-	*prev_was_redir = 0;
 	*curr_token = NULL;
 	if (!head_cmd)
 		return (return_error(1, "something went wrong", 0, -1));
@@ -103,10 +101,7 @@ int	check_if_first_call(t_cmd *head_cmd, t_cmd **prev_cmd, t_token **curr_token,
 	{
 		*prev_cmd = head_cmd;
 		if (!head_cmd->start)
-		{
-			*prev_was_redir = 1;
 			return (0);
-		}
 		*curr_token = head_cmd->start;
 		return (1);
 	}
@@ -123,30 +118,25 @@ t_token	*get_next_token(t_infos *infos, t_cmd *head_cmd,
 		t_cmd **prev_cmd, t_token *prev_token)
 {
 	t_token	*curr_token;
-	int		prev_was_redir;
 
 	if (!infos || !head_cmd)
 		return ((t_token *)return_null_error(1, "something went wrong", 0));
-	if (check_if_first_call(head_cmd, prev_cmd, &curr_token, &prev_was_redir) != 0)
+	if (check_if_first_call(head_cmd, prev_cmd, &curr_token) != 0)
 		return (curr_token);
 	else if (prev_token && prev_token != (*prev_cmd)->end)
 	{
-		printf("here with %s\n", prev_token->token);
-		if (!prev_token->prev || !prev_token->prev->linked_to_next)
+		if (!prev_token->prev || !prev_token->linked_to_next)
 			return (prev_token->next);
 		else if (prev_token->next != (*prev_cmd)->end)
 			return (prev_token->next->next);
-		prev_was_redir = 1;
 	}
 	else if (prev_token && check_if_end_pipeline(*prev_cmd, prev_token) == 1)
 		return (NULL);
-	else if (prev_token && prev_token == (*prev_cmd)->end && (*prev_cmd)->next_operator > 0)
-		prev_was_redir = 1;
 	*prev_cmd = (*prev_cmd)->next;
 	if (!*prev_cmd)
 		return (NULL);
 	curr_token = (*prev_cmd)->start;
-	if (get_next_token_loop(prev_cmd, &curr_token, prev_was_redir) == -1)
+	if (get_next_token_loop(prev_cmd, &curr_token) == -1)
 		return (NULL);
 	return (curr_token);
 }

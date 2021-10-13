d779fbd96a6fe0f9e29489d743097506433a8b97
Hind <hlichir@icloud.com>
2021-10-13 11:52:04 +0200

    string manipulation fix

--- parsing/tokenizer_utils.c
+++ parsing/tokenizer_utils.c
@@ -6,7 +6,7 @@
 /*   By: hlichir <hlichir@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 14:12:00 by anadege           #+#    #+#             */
-/*   Updated: 2021/10/13 00:51:43 by hlichir          ###   ########.fr       */
+/*   Updated: 2021/10/13 11:50:50 by hlichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -131,8 +131,9 @@ void	strings_manipulation(t_token **tokens)
 	while (curr_token)
 	{
 		to_compare = curr_token->token[curr_token->length];
-		if (curr_token->type == VARIABLE)
-			to_compare = curr_token->token[curr_token->length];
+		if (curr_token->type == VARIABLE 
+			&& curr_token->next && curr_token->next->type != VARIABLE)
+			to_compare = curr_token->token[curr_token->length + 1];
 		if (curr_token->type != STRING && curr_token->type != VARIABLE
 			&& curr_token->token[curr_token->length] && curr_token->next
 			&& ft_strchr("\"\'$", to_compare))

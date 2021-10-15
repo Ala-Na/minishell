3ceacd7870046917d41edfacac2fc3a856f99929
Hind Lichir <hlichir@student.42.fr>
2021-10-15 15:28:43 +0200

    fixed

--- builtins/pwd.c
+++ builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: hlichir < hlichir@student.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/06 14:50:47 by hlichir           #+#    #+#             */
-/*   Updated: 2021/10/14 12:37:32 by anadege          ###   ########.fr       */
+/*   Updated: 2021/10/15 15:28:19 by hlichir          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ char	*get_str_pwd(t_infos *infos)
 	int		pos;
 
 	pos = seek_elem_pos(infos->env, "PWD");
-	if (pos == -2)
-		return (return_null_error(1, "something went wrong", 0));
-	else if (pos == -1)
+	if (pos < 0)
 		return (NULL);
 	str = get_elem_value((infos->env)[pos]);
 	if (!str)
@@ -35,11 +33,19 @@ char	*get_str_pwd(t_infos *infos)
 */
 int	show_current_dir(t_infos *infos, t_cmd *cmd)
 {
-	char	*str;
+	char		*str;
+	struct stat	buf;
 
 	str = get_str_pwd(infos);
-	if (!str)
-		return (-1);
+	if ((str && (stat(str, &buf) == -1 || !S_ISDIR(buf.st_mode)))
+		|| !str)
+	{
+		if (str)
+			free(str);
+		str = get_curr_dir(infos, 0);
+		if (!str)
+			return (-1);
+	}
 	write(cmd->fd_output, str, ft_strlen(str));
 	write(cmd->fd_output, "\n", 1);
 	free(str);
